f50b77e500b222a9683f298c2d83cb617abb6b05
Hakim0122 <bustanulhakim81@gmail.com>
2024-12-20 22:22:49 +0700

    Modifikasi halaman masuk dan daftar

--- daftar.php
+++ daftar.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require 'function.php';
 
 if (isset($_POST['daftar'])) {
@@ -19,37 +19,51 @@ if (isset($_POST['daftar'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>daftar</title>
+  <title>Daftar akun?</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
+
 <body>
-  <h3>Daftar</h3>
-  <form action="" method="POST">
-    <ul>
-      <li>
-        <label>
-          Username :
-          <input type="text" name="username" autofocus autocomplete="off" required>
-        </label>
-      </li>
-      <li>
-        <label>
-          Password :
-          <input type="password" name="password1" required>
-        </label>
-      </li>
-      <li>
-        <label>
-          Konfirmasi Password :
-          <input type="password" name="password2" required>
-        </label>
-      </li>
-      <li>
-        <button type="submit" name="daftar">Daftar</button>
-      </li>
-    </ul>
-  </form>
+  <div class="home">
+    <div class="wrapper-daftar">
+      <div class="form-box daftar">
+        <h2>Daftar</h2>
+        <form action="" method="POST">
+          <div class="input-box">
+            <span class="icon"><ion-icon name="person-outline"></ion-icon></span>
+            <input type="text" name="username" autofocus autocomplete="off" required>
+            <label>Username</label>
+          </div>
+          <div class="input-box">
+            <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
+            <input type="password" name="password1" required>
+            <label>Password</label>
+          </div>
+          <div class="input-box">
+            <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
+            <input type="password" name="password2" required>
+            <label>Konfirmasi Password</label>
+          </div>
+          <div class="ingat-saya">
+            <label><input type="checkbox" required >Saya setuju dengan <a href="">persyaratannya</a></label>
+          </div>
+          <button type="submit" class="btn" name="daftar">Daftar</button>
+          <div class="daftar-sekarang">
+            <p>Sudah memiliki akun?<a href="login.php" class="login-link"> Masuk</a></p>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 session_start();
-if(isset($_SESSION['login'])){
+if (isset($_SESSION['login'])) {
   header("Location: index.php");
 }
 
@@ -15,39 +15,52 @@ if (isset($_POST['login'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Masuk</title>
+  <title>Masuk?</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
+
 <body>
-  <h3>Masuk</h3>
+  <div class="home">
+    <div class="wrapper">
+      <div class="form-box login"></div>
+      <h2>Masuk</h2>
 
-  <?php if (isset($login['error'])): ?>
-    <script>
-      alert("<?php echo $login['pesan']; ?>");
-    </script>
-  <?php endif; ?>
+      <?php if (isset($login['error'])): ?>
+        <script>
+          alert("<?php echo $login['pesan']; ?>");
+        </script>
+      <?php endif; ?>
 
-  <form action="" method="POST">
-    <ul>
-      <li>
-        <label>
-          Username :
-          <input type="text" name="username" autofocus autocomplete="off" required>
-        </label>
-      </li>
-      <li>
-        <label>
-          Password :
+      <form action="" method="POST">
+        <div class="input-box">
+          <span class="icon"><ion-icon name="person-outline"></ion-icon></span>
+          <input type="text" name="username" required>
+          <label>Username</label>
+        </div>
+        <div class="input-box">
+          <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
           <input type="password" name="password" required>
-        </label>
-      </li>
-      <li>
-        <button type="submit" name="login">Masuk</button>
-        <button><a href="daftar.php">Daftar</a></button>
-      </li>
-    </ul>
-  </form>
+          <label>Kata sandi</label>
+        </div>
+        <div class="ingat-saya">
+          <label><input type="checkbox" >Ingat saya</label>
+          <a href="">Lupa password?</a>
+        </div>
+        <button type="submit" name="login" class="btn">Masuk</button>
+        <div class="daftar-sekarang">
+          <p>Belum memiliki akun?<a href="daftar.php" class="daftar-link"> Daftar</a></p>
+        </div>
+      </form>
+    </div>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
+
 </html>
